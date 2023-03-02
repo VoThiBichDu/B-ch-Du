6159b66a71632978e9b41999b592d7582584fd89
Bich Du <bbbichdu0107@gmail.com>
2023-03-02 11:30:10 +0700

    first commit

---
+++ WEB/WebApplication1/Controllers/CatagoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CatagoryController : Controller
+    {
+        // GET: Catagory
+        public ActionResult Catagory()
+        {
+            return View();
+        }
+    }
+}
